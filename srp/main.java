bba559ff61b9e1017bf3c55fc6df8df8c54d0b41
tagaism <tagai.ismailov@gmail.com>
2025-11-22 21:42:33 +0600

    DP SRP

---
+++ srp/main.java
@@ -0,0 +1,11 @@
+package srp;
+
+public class main {
+    public static void main(String[] args) {
+        Journal j = new Journal();
+        j.addEntry("I woke up at 7 o'clock today.");
+        j.addEntry("I ride a JR Central Line to get to work.");
+        System.out.println(j);
+
+    }
+}
